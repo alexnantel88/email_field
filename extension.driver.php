51a1c4c0ba3a480b85f5849164cc42ce2525ccf9
Nick Dunn <nick@nick-dunn.co.uk>
2011-03-22 19:00:09 +0000

    Removed old accessors, converted README to Markdown and bumped version to 1.1

--- extension.driver.php
+++ extension.driver.php
@@ -4,20 +4,21 @@
 	
 		public function about(){
 			return array('name' => 'Field: Email',
-						 'version' => '1.0',
-						 'release-date' => '2009-09-03',
-						 'author' => array('name' => 'Alistair Kearney',
-										   'website' => 'http://www.pointybeard.com',
-										   'email' => 'alistair@symphony-cms.com')
-				 		);
+						 'version' => '1.1',
+						 'release-date' => '2011-03-22',
+						 'author' => array(
+							'name' => 'Symphony Team',
+							'website' => 'http://symphony-cms.com'
+						)
+				 	);
 		}
 		
 		public function uninstall(){
-			$this->_Parent->Database->query("DROP TABLE `tbl_fields_email`");
+			Symphony::Database()->query("DROP TABLE `tbl_fields_email`");
 		}
 
 		public function install(){
-			return $this->_Parent->Database->query("CREATE TABLE `tbl_fields_email` (
+			return Symphony::Database()->query("CREATE TABLE `tbl_fields_email` (
 			  `id` int(11) unsigned NOT NULL auto_increment,
 			  `field_id` int(11) unsigned NOT NULL,
 			  PRIMARY KEY  (`id`),
@@ -26,4 +27,3 @@
 		}
 			
 	}
-
