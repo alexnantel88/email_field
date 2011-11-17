3adc1d4908cd61b8670f343ddceeefc07b1a41e5
Brendan Abbott <brendan@bloodbone.ws>
2011-11-17 14:46:05 +1000

    Set ENGINE to MyISAM and table to UTF8

--- extension.driver.php
+++ extension.driver.php
@@ -1,29 +1,32 @@
 <?php
 
 	Class extension_Email_Field extends Extension{
-	
+
 		public function about(){
-			return array('name' => 'Field: Email',
-						 'version' => '1.1',
-						 'release-date' => '2011-03-22',
-						 'author' => array(
-							'name' => 'Symphony Team',
-							'website' => 'http://symphony-cms.com'
-						)
-				 	);
+			return array(
+				'name' => 'Field: Email',
+				'version' => '1.1',
+				'release-date' => '2011-03-22',
+				'author' => array(
+					'name' => 'Symphony Team',
+					'website' => 'http://symphony-cms.com'
+				)
+			);
 		}
-		
+
 		public function uninstall(){
 			Symphony::Database()->query("DROP TABLE `tbl_fields_email`");
 		}
 
 		public function install(){
-			return Symphony::Database()->query("CREATE TABLE `tbl_fields_email` (
-			  `id` int(11) unsigned NOT NULL auto_increment,
-			  `field_id` int(11) unsigned NOT NULL,
-			  PRIMARY KEY  (`id`),
-			  KEY `field_id` (`field_id`)
-			);");
+			return Symphony::Database()->query("
+				CREATE TABLE `tbl_fields_email` (
+					`id` int(11) unsigned NOT NULL auto_increment,
+					`field_id` int(11) unsigned NOT NULL,
+					PRIMARY KEY  (`id`),
+					KEY `field_id` (`field_id`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+			");
 		}
-			
+
 	}
--- fields/field.email.php
+++ fields/field.email.php
@@ -1,20 +1,20 @@
 <?php
-	
+
 	require_once(TOOLKIT . '/fields/field.input.php');
-	
+
 	Class fieldEmail extends fieldInput {
-		
+
 		public function __construct(&$parent){
 			parent::__construct($parent);
 			$this->_name = __('Email');
 			$this->_required = true;
-			
+
 			$this->set('required', 'no');
 		}
-		
+
 		public function buildDSRetrivalSQL($data, &$joins, &$where, $andOperation = false) {
 			$field_id = $this->get('id');
-			
+
 			if (self::isFilterRegex($data[0])) {
 				$this->_key++;
 				$pattern = str_replace('regexp:', '', $this->cleanValue($data[0]));
@@ -26,7 +26,7 @@
 				$where .= "
 					AND t{$field_id}_{$this->_key}.value REGEXP '{$pattern}'
 				";
-				
+
 			} elseif ($andOperation) {
 				foreach ($data as $value) {
 					$this->_key++;
@@ -40,14 +40,14 @@
 						AND t{$field_id}_{$this->_key}.value = '{$value}'
 					";
 				}
-				
+
 			} else {
 				if (!is_array($data)) $data = array($data);
-				
+
 				foreach ($data as &$value) {
 					$value = $this->cleanValue($value);
 				}
-				
+
 				$this->_key++;
 				$data = implode("', '", $data);
 				$joins .= "
@@ -59,47 +59,43 @@
 					AND t{$field_id}_{$this->_key}.value IN ('{$data}')
 				";
 			}
-			
+
 			return true;
 		}
 
-		private function __applyValidationRule($data){			
+		private function __applyValidationRule($data){
 			include(TOOLKIT . '/util.validators.php');
 			$rule = (isset($validators['email']) ? $validators['email'] : '/^\w(?:\.?[\w%+-]+)*@\w(?:[\w-]*\.)+?[a-z]{2,}$/i');
 			return General::validateString($data, $rule);
 		}
-		
-		public function setFromPOST($postdata){
-			parent::setFromPOST($postdata);
-		}
-		
+
 		public function checkPostFieldData($data, &$message, $entry_id=NULL){
 
 			$message = NULL;
-			
+
 			if($this->get('required') == 'yes' && strlen($data) == 0){
 				$message = __("'%s' is a required field.", array($this->get('label')));
 				return self::__MISSING_FIELDS__;
-			}	
-			
+			}
+
 			if(!$this->__applyValidationRule($data)){
 				$message = __("'%s' is not a valid email address.", array($this->get('label')));
-				return self::__INVALID_FIELDS__;	
+				return self::__INVALID_FIELDS__;
 			}
 
 			return self::__OK__;
-							
+
 		}
-		
+
 		public function processRawFieldData($data, &$status, $simulate = false, $entry_id = null) {
 			$status = self::__OK__;
-			
+
 			if (strlen(trim($data)) == 0) return array();
-			
+
 			$result = array(
 				'value' => $data
 			);
-			
+
 			return $result;
 		}
 
@@ -107,40 +103,38 @@
 			$value = $data['value'];
 
 			if($encode === true) $value = General::sanitize($value);
-			
+
 			$wrapper->appendChild(
 				new XMLElement(
 					$this->get('element_name'), $value, array('hash' => md5($data['value']))
 				)
 			);
 		}
-		
+
 		public function commit(){
 			if(!parent::commit()) return false;
-			
+
 			$id = $this->get('id');
 			if($id === false) return false;
-			
+
 			$fields = array();
 			$fields['field_id'] = $id;
-			
+
 			Symphony::Database()->query("DELETE FROM `tbl_fields_".$this->handle()."` WHERE `field_id` = '$id' LIMIT 1");
 			return Symphony::Database()->insert($fields, 'tbl_fields_' . $this->handle());
 		}
-		
+
 		public function displaySettingsPanel(&$wrapper, $errors = null) {
-			parent::displaySettingsPanel($wrapper, $errors);	
-			
+			parent::displaySettingsPanel($wrapper, $errors);
+
 			$div = new XMLElement('div', NULL, array('class' => 'compact'));
 			$this->appendRequiredCheckbox($div);
 			$this->appendShowColumnCheckbox($div);
 			$wrapper->appendChild($div);
 		}
-		
+
 		public function createTable(){
-			
 			return Symphony::Database()->query(
-			
 				"CREATE TABLE IF NOT EXISTS `tbl_entries_data_" . $this->get('id') . "` (
 				  `id` int(11) unsigned NOT NULL auto_increment,
 				  `entry_id` int(11) unsigned NOT NULL,
@@ -148,9 +142,8 @@
 				  PRIMARY KEY  (`id`),
 				  KEY `entry_id` (`entry_id`),
 				  KEY `value` (`value`)
-				) TYPE=MyISAM;"
-			
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
 			);
-		}		
+		}
 
 	}
